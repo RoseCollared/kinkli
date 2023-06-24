b053e5d495d477e0baeec5371474ac884fc369b0
RoseCollared <rosecollar3d@gmail.com>
2023-06-24 22:57:01 +0200

    do some styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { InputHTMLAttributes } from "react";
 import kinks from "../public/kinks.json";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-12 p-24">
-      <h1 className="text-5xl font-semibold">Kinklist</h1>
-      <form className="flex flex-col flex-wrap gap-8">
+    <main className="flex min-h-screen flex-col items-center gap-12 py-12 sm:py-16">
+      <h1 className="text-4xl font-bold drop-shadow-sm sm:text-5xl">
+        Kinklist
+      </h1>
+      <form className="flex w-full flex-col flex-wrap gap-8 overflow-auto px-8 sm:max-h-[3000px] sm:px-12">
         {Object.entries(kinks).map(([sectionName, questions]) => (
           <Section
             key={sectionName}
@@ -19,11 +22,15 @@ export default function Home() {
 
 function Section({ sectionName, questions }) {
   return (
-    <section className="flex flex-col gap-4">
-      <h2 className="text-3xl font-medium">{sectionName}</h2>
-      {Object.keys(questions).map((questionName) => (
-        <Question key={questionName} questionName={questionName} />
-      ))}
+    <section className="flex flex-col gap-4 rounded-xl border-2 border-rose-300 p-6 shadow-xl shadow-rose-100">
+      <h2 className="text-2xl font-semibold drop-shadow-sm sm:text-2xl lg:text-3xl">
+        {sectionName}
+      </h2>
+      <div className="flex flex-col gap-6">
+        {Object.keys(questions).map((questionName) => (
+          <Question key={questionName} questionName={questionName} />
+        ))}
+      </div>
     </section>
   );
 }
@@ -31,14 +38,26 @@ function Section({ sectionName, questions }) {
 function Question({ questionName }) {
   return (
     <fieldset>
-      <legend className="mb-2">{questionName}</legend>
-      <div className="flex space-x-4">
-        <input type="radio" name={questionName} />
-        <input type="radio" name={questionName} />
-        <input type="radio" name={questionName} />
-        <input type="radio" name={questionName} />
-        <input type="radio" name={questionName} />
+      <legend className="mb-4 text-lg font-medium leading-tight text-gray-600">
+        {questionName}
+      </legend>
+      <div className="flex gap-4">
+        <Radio name={questionName} />
+        <Radio name={questionName} />
+        <Radio name={questionName} />
+        <Radio name={questionName} />
+        <Radio name={questionName} />
       </div>
     </fieldset>
+  );
+}
+
+function Radio(props: InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      type="radio"
+      className="relative h-5 w-5 appearance-none rounded-full border-2 border-gray-400 before:absolute before:inset-0.5 before:rounded-full before:bg-rose-400 before:opacity-0 before:transition-opacity checked:before:opacity-100 hover:before:opacity-70 checked:hover:before:opacity-100"
+      {...props}
+    />
   );
 }
