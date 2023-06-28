0c728272e60ca75b89389b23dc20522044f85333
RoseCollared <rosecollar3d@gmail.com>
2023-06-28 22:08:01 +0200

    create use search param state hook

---
+++ src/hooks/use-search-param-state.ts
@@ -0,0 +1,25 @@
+"use client"
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export function useSearchParamState(
+  name: string,
+  initialValue?: string
+): ReturnType<typeof useState<string | null | undefined>> {
+  const router = useRouter();
+  const pathname = usePathname();
+  const currentParams = useSearchParams();
+
+  const [state, setState] = useState<string | null | undefined>(
+    currentParams.get(name) ?? initialValue
+  );
+
+  useEffect(() => {
+    const params = new URLSearchParams(currentParams.toString());
+    if (state) params.set(name, state);
+    router.replace(pathname + "?" + params.toString());
+  }, [state, name, router, pathname, currentParams]);
+
+  return [state, setState];
+}
