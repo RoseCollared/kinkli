7cee7cb47b0e831c2093a334f033ad565e59f205
RoseCollared <rosecollar3d@gmail.com>
2023-07-02 22:20:08 +0200

    format code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Kinklist',
-  description: 'The kinkiest kinklist of all kinklists',
-}
+  title: "Kinklist",
+  description: "The kinkiest kinklist of all kinklists",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,2 +1,2 @@
-export * from './form';
-export * from './schema';
+export * from "./form";
+export * from "./schema";
