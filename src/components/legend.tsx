c2df9f3e9ef464f8248201c558710e0df50ffa9b
RoseCollared <rosecollar3d@gmail.com>
2023-07-11 21:04:01 +0200

    change legend style when sticky

--- src/components/legend.tsx
+++ src/components/legend.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from "react";
+import { useInView } from "react-intersection-observer";
 import { twMerge } from "tailwind-merge";
 import { Radio } from "./radio";
 
@@ -6,46 +6,50 @@ interface LegendProps {
   className?: string;
 }
 
-export const Legend = forwardRef<HTMLElement, LegendProps>(
-  ({ className }, ref) => {
-    return (
-      <aside
-        ref={ref}
-        className={twMerge(
-          "z-10 flex flex-wrap justify-center gap-4 rounded-b-3xl bg-white p-4 shadow-lg shadow-rose-100",
-          className
-        )}
-      >
-        <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
-          <Radio value="7" checked readOnly />
-          Favorite
-        </label>
-        <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
-          <Radio value="6" checked readOnly />
-          Love
-        </label>
-        <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
-          <Radio value="5" checked readOnly />
-          Like
-        </label>
-        <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
-          <Radio value="4" checked readOnly />
-          Curious
-        </label>
-        <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
-          <Radio value="3" checked readOnly />
-          Meh
-        </label>
-        <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
-          <Radio value="2" checked readOnly />
-          Maybe
-        </label>
-        <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
-          <Radio value="1" checked readOnly />
-          Limit
-        </label>
-      </aside>
-    );
-  }
-);
-Legend.displayName = "Legend";
+export function Legend({ className }: LegendProps) {
+  const { ref, inView } = useInView({
+    threshold: 1,
+    rootMargin: "-1px", // to trigger when the element is within 1 px from the edge of the screen
+  });
+  const isSticky = !inView;
+
+  return (
+    <aside
+      ref={ref}
+      className={twMerge(
+        "z-10 flex flex-wrap justify-center gap-4 rounded-b-3xl bg-transparent px-8 py-4 transition-all",
+        isSticky && "border-2 border-t-0 border-rose-300 bg-white shadow-lg",
+        className
+      )}
+    >
+      <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
+        <Radio value="7" checked readOnly />
+        Favorite
+      </label>
+      <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
+        <Radio value="6" checked readOnly />
+        Love
+      </label>
+      <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
+        <Radio value="5" checked readOnly />
+        Like
+      </label>
+      <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
+        <Radio value="4" checked readOnly />
+        Curious
+      </label>
+      <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
+        <Radio value="3" checked readOnly />
+        Meh
+      </label>
+      <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
+        <Radio value="2" checked readOnly />
+        Maybe
+      </label>
+      <label className="flex items-center gap-2 text-lg font-medium text-gray-600">
+        <Radio value="1" checked readOnly />
+        Limit
+      </label>
+    </aside>
+  );
+}
