daa829a8daf6f230c6c96d31df23322d99d5a873
RoseCollared <rosecollar3d@gmail.com>
2023-07-11 21:52:57 +0200

    uncheck radio button when clicking it again

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -69,6 +69,7 @@ export const Form = forwardRef<
     </FormProvider>
   );
 });
+
 Form.displayName = "Form";
 
 interface SectionProps {
--- src/components/radio.tsx
+++ src/components/radio.tsx
@@ -1,25 +1,43 @@
 "use client";
 
 import { forwardRef, type InputHTMLAttributes } from "react";
+import { useFormContext, UseFormReturn } from "react-hook-form";
 import { twMerge } from "tailwind-merge";
+
+const colorMap = {
+  "1": "bg-red-100 before:bg-red-500", // 🟥
+  "2": "bg-orange-100 before:bg-orange-400", // 🟧
+  "3": "bg-yellow-100 before:bg-yellow-500", // 🟨
+  "4": "bg-green-100 before:bg-green-400", // 🟩
+  "5": "bg-sky-100 before:bg-sky-400", // 🟦
+  "6": "bg-violet-100 before:bg-violet-500", // 🟪
+  "7": "bg-pink-100 before:bg-pink-400", // 🩷
+};
 
 export const Radio = forwardRef<
   HTMLInputElement,
   InputHTMLAttributes<HTMLInputElement>
 >((props, ref) => {
-  const colorMap = {
-    "1": "bg-red-100 before:bg-red-500", // 😬
-    "2": "bg-orange-100 before:bg-orange-400", // 😟
-    "3": "bg-yellow-100 before:bg-yellow-500", // 🫤
-    "4": "bg-green-100 before:bg-green-400", // 🤔
-    "5": "bg-sky-100 before:bg-sky-400", // 😏
-    "6": "bg-violet-100 before:bg-violet-500", // 🥰
-    "7": "bg-pink-100 before:bg-pink-400", // 🥵
-  };
+  // This will be undefined when not used in a <FormProvider>
+  const formContext = useFormContext() as UseFormReturn | undefined;
+
+  const valueOfCheckedInput =
+    formContext && props.name ? formContext.watch(props.name) : undefined;
+  const isChecked =
+    props.value !== undefined && props.value === valueOfCheckedInput;
+
   return (
     <input
       type="radio"
       ref={ref}
+      onClick={() => {
+        // If the radio input is clicked while checked, we uncheck it
+        // This goes against standard browser behavior, but I think it's the
+        // most elegant way to remove the answer from an individual question
+        if (isChecked && props.name && formContext) {
+          formContext.setValue(props.name, null);
+        }
+      }}
       {...props}
       className={twMerge(
         "relative h-5 w-5 appearance-none rounded-full border-2 border-gray-400 transition-colors before:absolute before:inset-0.5 before:rounded-full before:bg-rose-400 before:opacity-0 before:transition-opacity checked:border-gray-500 checked:before:opacity-100 hover:before:opacity-70 checked:hover:before:opacity-100",
@@ -29,4 +47,5 @@ export const Radio = forwardRef<
     />
   );
 });
+
 Radio.displayName = "Radio";
