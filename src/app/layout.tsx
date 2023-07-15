4b496a45ad1592d84bebaa6cf8d6977ba00bd2c0
RoseCollared <rosecollar3d@gmail.com>
2023-07-15 21:45:36 +0200

    move main element to a place where it makes sense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,17 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`bg-rose-50 text-gray-800 ${inter.className}`}>
-        <main className="flex min-h-screen flex-col items-center">
-          <header className="flex w-full justify-center gap-8 px-12 pb-2 pt-12 sm:pt-16">
-            <h1 className="text-4xl font-bold drop-shadow-sm xs:text-5xl">
-              Kinklist
-            </h1>
-          </header>
-          <Legend className="sm:sticky sm:top-0" />
-          {children}
-          <BackToTopButton />
-        </main>
+      <body
+        className={`flex min-h-screen flex-col items-center bg-rose-50 text-gray-800 ${inter.className}`}
+      >
+        <header className="flex w-full justify-center gap-8 px-12 pb-2 pt-12 sm:pt-16">
+          <h1 className="text-4xl font-bold drop-shadow-sm xs:text-5xl">
+            Kinklist
+          </h1>
+        </header>
+        <Legend className="sm:sticky sm:top-0" />
+        <main>{children}</main>
+        <BackToTopButton />
       </body>
     </html>
   );
