72cdfec73dbb7f33de1242a004e193d70c93b0bb
RoseCollared <rosecollar3d@gmail.com>
2023-07-22 22:48:26 +0200

    add button icon

--- src/app/results/layout.tsx
+++ src/app/results/layout.tsx
@@ -2,6 +2,7 @@ import { BackToTopButton } from "@kinklist/components/back-to-top-button";
 import { Button } from "@kinklist/components/button";
 import { Legend } from "@kinklist/components/legend";
 import Link from "next/link";
+import { BiLeftArrowAlt } from "react-icons/bi";
 
 export default function ResultsLayout({
   children,
@@ -17,6 +18,7 @@ export default function ResultsLayout({
               variant="tertiary"
               className="px-3 py-2 text-base sm:px-3 sm:text-base"
             >
+              <Button.Icon Component={BiLeftArrowAlt} className="w-6 h-6" />
               Start from scratch...
             </Button>
           </Link>
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,12 @@
 import { ButtonHTMLAttributes } from "react";
+import { IconType } from "react-icons";
 import { twMerge } from "tailwind-merge";
 
 export type ButtonVariant = keyof typeof variantStyles;
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariant;
+  Icon?: IconType;
 }
 
 const variantStyles = {
@@ -15,20 +17,34 @@ const variantStyles = {
     "text-rose-500 hover:bg-rose-100 active:bg-rose-200 active:text-rose-600 shadow-none",
 } as const;
 
-export function Button({
-  variant = "primary",
-  className,
-  ...restProps
-}: ButtonProps) {
+export function Button(props: ButtonProps) {
+  const { variant = "primary", className, children, ...restProps } = props;
   return (
     <button
       type="button"
       {...restProps}
       className={twMerge(
-        "whitespace-nowrap rounded-lg px-4 py-2.5 text-lg font-semibold leading-none shadow transition-colors focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-sky-300 sm:px-6 sm:text-xl",
+        "whitespace-nowrap rounded-lg px-4 py-2.5 text-lg font-semibold leading-none shadow transition-colors focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-sky-300 sm:px-5 sm:text-xl",
         variantStyles[variant],
         className
       )}
+    >
+      <div className="flex items-center gap-2">{children}</div>
+    </button>
+  );
+}
+
+function ButtonIcon({
+  Component,
+  className,
+}: {
+  Component: IconType;
+  className?: string;
+}) {
+  return (
+    <Component
+      className={twMerge("h-4 w-4 shrink-0 sm:h-6 sm:w-6", className)}
     />
   );
 }
+Button.Icon = ButtonIcon;
