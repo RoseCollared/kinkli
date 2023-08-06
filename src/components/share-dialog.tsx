1fb956ac5c2c308c5237764b47756a24b41ccac2
RoseCollared <rosecollar3d@gmail.com>
2023-08-06 17:20:14 +0200

    add description for each share option

--- src/components/share-dialog.tsx
+++ src/components/share-dialog.tsx
@@ -3,42 +3,26 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import type { ReactNode } from "react";
-import { BiCheck, BiCopyAlt, BiImage, BiLink, BiX } from "react-icons/bi";
+import { BiCheck, BiCopy, BiImage, BiLinkExternal, BiX } from "react-icons/bi";
 import useClipboard from "react-use-clipboard";
-import type { UrlObject } from "url";
 import { Button, IconButton } from "./button";
 import { Input } from "./input";
 
 export function ShareDialog() {
-  const searchParams = useSearchParams();
-
   return (
     <Dialog.Portal>
       <Dialog.Overlay className="fixed inset-0 z-10 bg-black/50" />
-      <Dialog.Content className="fixed left-[50%] top-[50%] z-10 flex max-h-[85vh] w-[90vw] max-w-md translate-x-[-50%] translate-y-[-50%] flex-col gap-4 overflow-y-auto rounded-xl border-2 border-rose-300 bg-rose-50 p-6 drop-shadow-2xl focus:outline-none xs:gap-4 xs:p-8 sm:max-w-2xl">
-        <Dialog.Title className="grow text-2xl font-medium sm:text-3xl">
+      <Dialog.Content className="fixed left-[50%] top-[50%] z-10 flex max-h-[85vh] w-[90vw] max-w-md translate-x-[-50%] translate-y-[-50%] flex-col gap-4 overflow-y-auto rounded-xl border-2 border-rose-300 bg-white p-6 drop-shadow-2xl focus:outline-none xs:gap-4 xs:p-8 sm:max-w-2xl">
+        <Dialog.Title className="grow text-2xl font-semibold sm:text-3xl">
           Share
         </Dialog.Title>
         <Dialog.Description>
-          Choose how you want to share your results.
+          We recommend sharing your results link, but you can also generate an
+          image.
         </Dialog.Description>
         <div className="flex flex-col gap-x-8 gap-y-8 sm:max-w-none sm:flex-row">
-          <ShareOption
-            href={{ pathname: "/results", search: searchParams.toString() }}
-          >
-            <BiLink aria-hidden className="h-12 w-12 fill-rose-400" />
-            Link
-          </ShareOption>
-          <ShareOption
-            href={{
-              pathname: "/results/export",
-              search: searchParams.toString(),
-            }}
-          >
-            <BiImage aria-hidden className="h-12 w-12 fill-rose-400" />
-            Image
-          </ShareOption>
+          <LinkOption />
+          <ImageOption />
         </div>
         <Dialog.Close asChild>
           <IconButton
@@ -52,22 +36,35 @@ export function ShareDialog() {
   );
 }
 
-interface ShareOptionProps {
-  href: UrlObject;
-  children?: ReactNode;
-}
-
-function ShareOption({ href, children }: ShareOptionProps) {
-  const url = `${window.location.origin}${href.pathname}?${href.search}`;
+function LinkOption() {
+  const searchParams = useSearchParams();
+  const url = `${window.location.origin}/results${
+    searchParams.toString() && `?${searchParams.toString()}`
+  }`;
   const [isCopied, copy] = useClipboard(url, { successDuration: 2000 });
 
   return (
     <div className="flex grow basis-0 flex-col gap-2">
-      <Link href={href} target="_blank" tabIndex={-1}>
-        <Button variant="secondary" className="w-full py-6 sm:py-12">
-          <div className="flex flex-col items-center text-base">{children}</div>
-        </Button>
-      </Link>
+      <h3 className="text-xl font-medium">Link</h3>
+      <Button
+        onClick={copy}
+        variant="secondary"
+        className="w-full py-6 sm:py-12"
+      >
+        <div className="flex flex-col items-center text-base">
+          {isCopied ? (
+            <>
+              <BiCheck aria-hidden className="h-12 w-12 fill-rose-400" />
+              Copied
+            </>
+          ) : (
+            <>
+              <BiCopy aria-hidden className="h-12 w-12 fill-rose-400" />
+              Copy link
+            </>
+          )}
+        </div>
+      </Button>
       <div className="flex items-center gap-2">
         <Input
           readOnly
@@ -75,24 +72,48 @@ function ShareOption({ href, children }: ShareOptionProps) {
           onFocus={(event) => event.target.select()}
           className="min-w-0 grow"
         />
-        <IconButton
-          onClick={copy}
-          icon={
-            isCopied ? (
-              <BiCheck className="h-5 w-5" />
-            ) : (
-              <BiCopyAlt className="h-4 w-4" />
-            )
-          }
-          aria-label={isCopied ? "Copied" : "Copy"}
-          title={isCopied ? "Copied" : "Copy"}
-          className={isCopied ? "p-1.5 sm:p-1.5" : "p-2 sm:p-2"}
-        />
+        <Link
+          href={{ pathname: "/results", search: searchParams.toString() }}
+          target="_blank"
+          tabIndex={-1}
+        >
+          <IconButton
+            icon={<BiLinkExternal className="h-4 w-4" />}
+            aria-label="Open link in new tab"
+            title="Open link in new tab"
+            className="p-2 sm:p-2"
+          />
+        </Link>
       </div>
+      <p className="px-2 text-gray-500">
+        A link is easier to view on mobile devices and more accessible to people
+        with vision impairments.
+      </p>
     </div>
   );
 }
 
-// LEFT HERE
-// TODO: add a little description under each share option (or tooltip on mobile?), secondary text color
-// TODO: add icon to indicate button opens in new tab
+function ImageOption() {
+  const searchParams = useSearchParams();
+
+  return (
+    <div className="flex grow basis-0 flex-col gap-2">
+      <h3 className="text-xl font-medium">Image</h3>
+      <Link
+        href={{ pathname: "/results/export", search: searchParams.toString() }}
+        target="_blank"
+        tabIndex={-1}
+      >
+        <Button variant="secondary" className="w-full py-6 sm:py-12">
+          <div className="flex flex-col items-center text-base">
+            <BiImage aria-hidden className="h-12 w-12 fill-rose-400" />
+            Generate image
+          </div>
+        </Button>
+      </Link>
+      <p className="px-2 text-gray-500">
+        An image is always the same no matter how you&apos;re viewing it.
+      </p>
+    </div>
+  );
+}
