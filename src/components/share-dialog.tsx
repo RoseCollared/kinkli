e806d908a373e01d70187ce95abd1058e089630f
RoseCollared <rosecollar3d@gmail.com>
2023-08-13 22:22:54 +0200

    remove link copy functionality from share dialog

--- src/components/share-dialog.tsx
+++ src/components/share-dialog.tsx
@@ -4,10 +4,8 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import { BiCheck, BiCopy, BiImage, BiLinkExternal, BiX } from "react-icons/bi";
-import useClipboard from "react-use-clipboard";
+import { BiImage, BiLinkExternal, BiX } from "react-icons/bi";
 import { Button, IconButton } from "./button";
-import { Input } from "./input";
 
 export function ShareDialog({ open }: { open: boolean }) {
   return (
@@ -76,53 +74,24 @@ export function ShareDialog({ open }: { open: boolean }) {
 
 function LinkOption() {
   const searchParams = useSearchParams();
-  const url = `${window.location.origin}/results${
-    searchParams.toString() && `?${searchParams.toString()}`
-  }`;
-  const [isCopied, copy] = useClipboard(url, { successDuration: 2000 });
 
   return (
     <div className="flex grow basis-0 flex-col gap-2">
       <h3 className="text-xl font-medium">Link</h3>
-      <Button
-        onClick={copy}
-        variant="secondary"
-        className="w-full py-6 sm:py-12"
+
+      <Link
+        href={{ pathname: "/results", search: searchParams.toString() }}
+        target="_blank"
+        tabIndex={-1}
       >
-        <div className="flex flex-col items-center text-base">
-          {isCopied ? (
-            <>
-              <BiCheck aria-hidden className="h-12 w-12 fill-rose-400" />
-              Copied
-            </>
-          ) : (
-            <>
-              <BiCopy aria-hidden className="h-12 w-12 fill-rose-400" />
-              Copy link
-            </>
-          )}
-        </div>
-      </Button>
-      <div className="flex items-center gap-2">
-        <Input
-          readOnly
-          defaultValue={url}
-          onFocus={(event) => event.target.select()}
-          className="min-w-0 grow"
-        />
-        <Link
-          href={{ pathname: "/results", search: searchParams.toString() }}
-          target="_blank"
-          tabIndex={-1}
-        >
-          <IconButton
-            icon={<BiLinkExternal className="h-4 w-4" />}
-            aria-label="Open link in new tab"
-            title="Open link in new tab"
-            className="p-2 sm:p-2"
-          />
-        </Link>
-      </div>
+        <Button variant="secondary" className="w-full py-6 sm:py-12">
+          <div className="flex flex-col items-center text-base">
+            <BiLinkExternal aria-hidden className="h-12 w-12 fill-rose-400" />
+            Open link
+          </div>
+        </Button>
+      </Link>
+
       <p className="px-2 text-gray-500">
         A link is easier to view on mobile devices and more accessible to people
         with vision impairments.
@@ -137,6 +106,7 @@ function ImageOption() {
   return (
     <div className="flex grow basis-0 flex-col gap-2">
       <h3 className="text-xl font-medium">Image</h3>
+
       <Link
         href={{ pathname: "/results/export", search: searchParams.toString() }}
         target="_blank"
@@ -149,6 +119,7 @@ function ImageOption() {
           </div>
         </Button>
       </Link>
+
       <p className="px-2 text-gray-500">
         An image is always the same no matter how you&apos;re viewing it.
       </p>
