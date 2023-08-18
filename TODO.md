54df8ecb13d017d55a369020d7f2220e0f8c7013
RoseCollared <rosecollar3d@gmail.com>
2023-08-18 22:05:49 +0200

    fix hydration error when loading the page with openParam === "open"

--- TODO.md
+++ TODO.md
@@ -1,5 +1,3 @@
 TODO
 
-- Change /results/export to /results/image?
-- Use latest Next.js version when hydration bug is fixed: https://github.com/vercel/next.js/issues/53110
 - Dark mode
--- src/components/share-button.tsx
+++ src/components/share-button.tsx
@@ -2,28 +2,36 @@
 
 import { useSearchParamState } from "@kinklist/hooks/use-search-param-state";
 import * as Dialog from "@radix-ui/react-dialog";
+import { useEffect, useState } from "react";
 import { BiSolidShareAlt } from "react-icons/bi";
 import { Button } from "./button";
 import { ShareDialog } from "./share-dialog";
 
 export function ShareButton() {
+  const [open, setOpen] = useState(false);
   const [openParam, setOpenParam] = useSearchParamState({
     name: "share-dialog",
     method: "push",
   });
 
-  // LEFT HERE
-  // FIXME: hydration error when loading the page with openParam === "open"
+  // Set the open state of the dialog in an effect to prevent hydration error
+  // when loading the page with openParam === "open"
+  useEffect(() => {
+    setOpen(openParam === "open");
+  }, [openParam]);
 
   return (
     <Dialog.Root
-      open={openParam === "open"}
+      open={open}
       onOpenChange={(open) => setOpenParam(open ? "open" : null)}
     >
       <Dialog.Trigger asChild>
         <Button icon={<BiSolidShareAlt />}>Share</Button>
       </Dialog.Trigger>
-      <ShareDialog open={openParam === "open"} />
+      {/* We pass an `open` prop to the dialog so we can render the content
+      conditionally, which is required to use <AnimatePresence /> to animate
+      the opening/closing of the dialog. */}
+      <ShareDialog open={open} />
     </Dialog.Root>
   );
 }
