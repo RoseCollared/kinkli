598718ef73faede76d7415b5fb872b4a7207d82b
RoseCollared <rosecollar3d@gmail.com>
2023-08-29 20:33:02 +0200

    animate copy button in results header

--- src/app/results/buttons.tsx
+++ src/app/results/buttons.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@kinklist/components/button";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { BiCheck, BiImage, BiLink } from "react-icons/bi";
@@ -9,18 +10,26 @@ import useClipboard from "react-use-clipboard";
 export function CopyButton() {
   const url = typeof window !== "undefined" ? window.location.href : "";
   const [isCopied, copy] = useClipboard(url, {
-    successDuration: 2000,
+    successDuration: 2500,
   });
 
   return (
-    <Button
-      icon={isCopied ? <BiCheck className="h-6 w-6" /> : <BiLink />}
-      onClick={() => copy()}
-    >
-      <span className="hidden xl:inline">
-        {isCopied ? "Copied" : "Copy link"}
-      </span>
-      <span className="inline xl:hidden">{isCopied ? "Copied" : "Copy"}</span>
+    // Not using Button `icon` prop because we need to animate it
+    <Button as={motion.button} layout onClick={() => copy()}>
+      <motion.div
+        layout
+        key={String(isCopied)}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.2 }}
+        className="flex items-center justify-center gap-2"
+      >
+        <span className="hidden xl:inline">
+          {isCopied ? "Copied" : "Copy link"}
+        </span>
+        <span className="inline xl:hidden">{isCopied ? "Copied" : "Copy"}</span>
+        {isCopied ? <BiCheck className="h-6 w-6" /> : <BiLink />}
+      </motion.div>
     </Button>
   );
 }
