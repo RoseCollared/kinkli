51a5b35b3537a09883c699702a7398fe8b9500ff
RoseCollared <rosecollar3d@gmail.com>
2023-09-01 21:16:20 +0200

    make title white on home page

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,20 +5,16 @@ import type { ReactNode } from "react";
 
 // This route group exists because the layout is different from the results page
 
-export default function HomeLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function HomeLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <header className="flex w-full items-center justify-center gap-8 px-12 pb-2 pt-12 sm:justify-between sm:pt-16">
         <div className="hidden grow sm:block">
           {/* Placeholder which grows to ensure title is centered */}
         </div>
-        <h1 className="text-5xl font-bold drop-shadow-sm dark:text-red-600">
-          Kinklist
-        </h1>
+
+        <h1 className="text-5xl font-bold drop-shadow-sm">Kinklist</h1>
+
         <div className="flex basis-0 justify-end sm:grow">
           <ShareButton />
         </div>
--- src/app/results/layout.tsx
+++ src/app/results/layout.tsx
@@ -7,7 +7,9 @@ export default function ResultsLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <header className="flex w-full flex-col items-center justify-center gap-x-8 gap-y-4 px-12 pt-12 sm:flex-row sm:justify-between sm:pb-2 sm:pt-16">
-        <div className="hidden basis-0 sm:block sm:grow" />
+        <div className="hidden basis-0 sm:block sm:grow">
+          {/* Placeholder which grows to ensure title is centered */}
+        </div>
 
         <h1 className="text-5xl font-bold drop-shadow-sm">Kinklist</h1>
 
