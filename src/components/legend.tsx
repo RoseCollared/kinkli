34de8380e647ffdfadb92d6ffe30d8cdf1059802
RoseCollared <rosecollar3d@gmail.com>
2023-09-02 17:00:31 +0200

    fix border transition on legend

--- src/components/legend.tsx
+++ src/components/legend.tsx
@@ -39,11 +39,11 @@ export function Legend({ showNA, className }: LegendProps) {
       ref={ref}
       className={twMerge(
         // Fine-tune max width to balance rows
-        "z-10 box-content flex max-w-sm flex-wrap justify-center gap-x-4 gap-y-1 rounded-b-3xl bg-transparent px-8 py-4 transition-all sm:sticky sm:top-0 md:max-w-none",
+        "z-10 box-content flex max-w-sm flex-wrap justify-center gap-x-4 gap-y-1 rounded-b-3xl bg-transparent px-8 py-4 transition-colors sm:sticky sm:top-0 sm:border-2 sm:border-t-0 sm:border-transparent md:max-w-none",
         showNA && "md:max-w-sm lg:max-w-none",
         isSticking &&
           !isExport &&
-          "sm:border-2 sm:border-t-0 sm:border-rose-300 sm:bg-white sm:shadow-lg dark:sm:border-red-700 dark:sm:bg-zinc-700",
+          "sm:border-rose-300 sm:bg-white sm:shadow-lg dark:sm:border-red-700 dark:sm:bg-zinc-700",
         className
       )}
     >
