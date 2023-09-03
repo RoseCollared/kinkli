edc83a159e93aac087fe8dafd90c9f38a62feb10
RoseCollared <rosecollar3d@gmail.com>
2023-09-03 22:34:07 +0200

    close share dialog when navigating back

--- src/hooks/use-search-param-state.ts
+++ src/hooks/use-search-param-state.ts
@@ -1,36 +1,52 @@
 "use client";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useMemo,
+  useState,
+} from "react";
 
 export interface Options {
   name: string;
-  initialValue?: string;
   method?: "push" | "replace";
 }
 
 export function useSearchParamState(
   options: Options
 ): ReturnType<typeof useState<string | null | undefined>> {
-  const { name, initialValue, method = "replace" } = options;
+  const { name, method = "replace" } = options;
 
   const router = useRouter();
   const pathname = usePathname();
-  const currentParams = useSearchParams();
+  const params = useSearchParams();
 
-  const [state, setState] = useState<string | null | undefined>(
-    currentParams.get(name) ?? initialValue
+  const state = useMemo(() => params.get(name), [params, name]);
+
+  const setState = useCallback<
+    Dispatch<SetStateAction<string | null | undefined>>
+  >(
+    (valueOrFn) => {
+      let newState: string | null | undefined;
+      if (typeof valueOrFn === "function") {
+        newState = valueOrFn(state);
+      } else {
+        newState = valueOrFn;
+      }
+
+      const newParams = new URLSearchParams(params.toString());
+      if (typeof newState === "string") {
+        newParams.set(name, newState);
+      } else {
+        newParams.delete(name);
+      }
+
+      router[method](pathname + "?" + newParams.toString(), { scroll: false });
+    },
+    [params, method, name, pathname, router, state]
   );
-
-  useEffect(() => {
-    const params = new URLSearchParams(currentParams.toString());
-    if (typeof state === "string") {
-      params.set(name, state);
-    } else {
-      params.delete(name);
-    }
-    router[method](pathname + "?" + params.toString(), { scroll: false });
-  }, [state, name, router, pathname, currentParams, method]);
 
   return [state, setState];
 }
